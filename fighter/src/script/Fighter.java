2797dfaf23e3df255f17aa9fdb3f706b55dcebea
Burak Inan <burakinan1@hotmail.com>
2019-04-20 15:11:59 +0200

    added fighter

---
+++ fighter/src/script/Fighter.java
@@ -0,0 +1,54 @@
+package script;
+
+import org.rspeer.runetek.adapter.component.Item;
+import org.rspeer.runetek.adapter.scene.Npc;
+import org.rspeer.runetek.adapter.scene.Player;
+import org.rspeer.runetek.api.commons.Time;
+import org.rspeer.runetek.api.component.Dialog;
+import org.rspeer.runetek.api.component.tab.Inventory;
+import org.rspeer.runetek.api.local.Health;
+import org.rspeer.runetek.api.scene.Npcs;
+import org.rspeer.runetek.api.scene.Players;
+import org.rspeer.script.Script;
+import org.rspeer.script.ScriptMeta;
+
+import java.util.function.Predicate;
+
+@ScriptMeta(name = "Fighter", desc = "fights", developer = "burak")
+public class Fighter extends Script {
+
+    private static final String FOOD = "Salmon";
+    private static final int MIN_EAT_HEALTH = 10;
+    private static final String EAT_ACTION = "Eat";
+    private static final String ATTACK_ACTION = "Attack";
+    private static final String NPC_NAME = "Zombie";
+    private static final Predicate<Npc> NPC_PRED = x -> x.getName().equals(NPC_NAME) && (x.getTargetIndex() == Players.getLocal().getIndex() || x.getTargetIndex() == -1);
+
+    @Override
+    public int loop() {
+        Item food = Inventory.getFirst(FOOD);
+        Player local = Players.getLocal();
+        Npc targetNpc = Npcs.getNearest(NPC_PRED);
+
+        if (food == null) {
+            return -1;
+        }
+
+        if (Dialog.canContinue()) {
+            Dialog.processContinue();
+        }
+
+        if (Health.getCurrent() <= MIN_EAT_HEALTH) {
+            food.interact(EAT_ACTION);
+        }
+
+        if (local.getTargetIndex() == -1
+                && targetNpc != null
+                && targetNpc.interact(ATTACK_ACTION)) {
+            Time.sleepUntil(() -> local.getTargetIndex() != -1, 5000);
+        }
+
+
+        return 600;
+    }
+}
